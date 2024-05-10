20d00a8a6d3ac276dcfcc46ac4a613e8f8d969b8
jj <j0bara@MacBook-Air-Jakub.local>
2024-05-10 20:38:13 +0200

    Added CheckUser endpoint

--- app/src/main/java/com/example/pumproject/databaseConnection/PlaceholderApi.kt
+++ app/src/main/java/com/example/pumproject/databaseConnection/PlaceholderApi.kt
@@ -1,8 +1,9 @@
 package com.example.pumproject.databaseConnection
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface PlaceholderApi {
-    @GET("user")
-    suspend fun user(): User
+    @GET("CheckUser?name={idname}")
+    suspend fun user(@Path("idname") idname : String): List<User>
 }
--- app/src/main/java/com/example/pumproject/databaseConnection/UserRepository.kt
+++ app/src/main/java/com/example/pumproject/databaseConnection/UserRepository.kt
@@ -2,7 +2,7 @@ package com.example.pumproject.databaseConnection
 
 class UserRepository {
     private val api = RetrofitInstance.api
-    suspend fun getUser() : User{
-        return api.user()
+    suspend fun getUser(name : String) : List<User>{
+        return api.user(name)
     }
 }
